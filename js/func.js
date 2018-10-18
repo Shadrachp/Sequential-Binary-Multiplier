5f2c23ad3a2a067ffa0db316493e2ad950f04dfb
Shadrach Gabriel Penano <33576257+Shadrachp@users.noreply.github.com>
2018-10-18 19:14:11 +0800

    Add files via upload

--- js/func.js
+++ js/func.js
@@ -1,6 +1,34 @@
 $(document).ready(()=>{
+    
    $("#btnSubmit").click((e)=>{
       e.preventDefault();
-      $("#output").append("<p>" + $("#txt_inputQ").val()+ "</p>");
+       
+      var output = "<p>"+ " M:" + $("#txt_inputM").val() +" Q:"  + $("#txt_inputQ").val()  +"&nbspA:#### </p>";
+       
+       
+       
+       if(isBinary($("#txt_inputQ").val()) != null && isBinary($("#txt_inputM").val()) != null)
+           $("#output").append(output);
    });
+    
+    
+    /**
+    * Checks if the input is binary
+    * @param input {String} - input of user either Q or M
+    * returns null if input is not binary.
+    */
+    function isBinary(input){
+        return input.match(/^[0-1]+$/);
+    }
+    
+     /**
+    * shifts each character to the right
+    * @param A {String} - 
+    * @param Q {String} - 
+    */
+    function shiftRight(A, Q){
+        
+    }
+    
+    
 });
