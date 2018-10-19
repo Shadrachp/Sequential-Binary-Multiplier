7772cb07a375600e826a00427beb0f61c420da1f
Shadrachp <shadrachgabriel@icloud.com>
2018-10-20 01:56:47 +0800

    Shift Right functions
    - Incomplete shift right function of the program

--- js/func.js
+++ js/func.js
@@ -1,5 +1,6 @@
 $(document).ready(()=>{
-    
+    var currA = [],
+        currQ = [];
    $("#btnSubmit").click((e)=>{
       e.preventDefault();
        
@@ -27,8 +28,49 @@ $(document).ready(()=>{
     * @param Q {String} - 
     */
     function shiftRight(A, Q){
-        
+        initCurrentVar(A, Q);
+        shiftingAlgorithm();
+        A = currA.slice();
+        Q = currQ.slice();
     }
     
+    /**
+    * Algorithm for shifting contents of two char[] var to the right
+    * Used variables:
+    * Global var currA {char[]}- Array of characters whose contents will be shifted to index + 1
+    * Global var currQ {char[]}- Array of characters whose contents will be shifted to index + 1
+    * tempQ {char[]] - a copy of currQ
+    * tempA {charp[]} - a copy of currA
+    * carry {char} - the carry bit or whatever you call it
+    */
+    function shiftingAlgorithm(){
+        // TODO: Make this more efficient later
+//        const tempQ = currQ.slice();
+//        const tempA = currA.slice();\
+        //incomplete loop
+//        for(var i = 0; i < tempA.length - 1; i++){
+//            currA[i + 1] = tempA[i];
+//            
+//        }
+    }
+    
+    /**
+    * Initializes the curr variables
+    * @param A {String} - data to be stored to currA var
+    * @param Q {String} - data to be stored to currQ var
+    */
+    function initCurrentVar(A, Q){
+        currA = splitString(A);
+        currQ = splitString(Q)
+    }
+    
+    /**
+    * splits the string into each char passed as parameter
+    * @param input {String} - string to be splitted
+    * returns an array of characters
+    */
+    function splitString(input){
+        return input.split('');
+    }
     
 });
