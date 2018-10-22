95d839ea87d5427b7f1102eb5bf431da875a7e68
Shadrachp <shadrachgabriel@icloud.com>
2018-10-23 06:21:58 +0800

    Buggy Shift Right

--- js/func.js
+++ js/func.js
@@ -1,17 +1,20 @@
 $(document).ready(()=>{
     var currA = [],
         currQ = [],
-        A = "", Q, M, C;
+        A, Q, M, C = '0';
    $("#btnSubmit").click((e)=>{
       e.preventDefault();      
        //TODO: should also clear output field
-       if(isBinary($("#txt_inputQ").val()) != null && isBinary($("#txt_inputM").val()) != null){
+       if(isBinary($("#txt_inputQ").val()) != null && isBinary($("#txt_inputM").val()) != null && isCountValid()){
+           clearOutput();
             Q = $("#txt_inputQ").val();
             M = $("#txt_inputM").val();
             currQ = splitString(Q);
             initA();
-            $("#output").append(createTag(A, M, Q));
+            $("#output").append(createElement(A, M, Q));
        }else{
+           clearOutput();
+           $("#output").append(createErrElement());
            //add some message that input is invalid
        }
    });
@@ -19,20 +22,31 @@ $(document).ready(()=>{
     //TODO: Create Next Button for .html file
      //executes 1 cycle.
     $("#btnNext").click((e)=>{
-        if(getLSB(Q) == '0' && C == '1'){
-            AddBin(A, M); //not yet working
+        if(getLSB(currQ) == '0' && C == '1'){
+//            AddBin(A, M); //not yet working
             shiftRight(A, Q, currA, currQ); //This will work after creating Shifting algorithm
-        }else if(getLSB(Q) == '1' && C == '0'){
-            SubtractBin(A. M); //after creating AddBin algo, this will work
+        }else if(getLSB(currQ) == '1' && C == '0'){
+//            SubtractBin(A, M); //after creating AddBin algo, this will work
             shiftRight(A, Q, currA, currQ); //This will work after creating Shifting algorithm
         }else shiftRight(A, Q, currA, currQ);
+        $("#output").append(createElement(A, M, Q));
     });
     
     //TODO: Create Clear button for .html file use the id below
     $("#btnClear").click((e)=>{
        //should clear the output field 
+        clearOutput();
     });
+    
+    function isCountValid(){
+        return $("#txt_inputM").val().length <= 16 &&
+            $("#txt_inputQ").val().length <= 16;
+    }
 
+    function clearOutput(){
+        $("#output").empty();
+    }
+    
     function AddBin(A, M){
         //TODO
         //AplusM algo
@@ -56,27 +70,27 @@ $(document).ready(()=>{
     *Initializes A variable
     */ 
     function initA(){
+        A = '';
         for(var i = 0; i < Q.length; i++) A += '0';
         currA = splitString(A);
     }
     
-    /**
-    * Creates the element for displaying data
-    * Used by the compute button
-    */
-    function createElement(A, M, Q){
-        return "<p>M:" +M+" Q:"  + Q  +"&nbspA:"+A+
-        "</p>";
-    }
      
     
     /**
     * Creates the element for displaying data
     * Used by the Next button
     */
-    function createElement(A, Q){
-        return "<p>M:" +M+" aQ:"  + Q  +"&nbspA:"+A+
+    function createElement(A, M, Q){
+        return "<p>M:" +M+" A:"  + A  +"&nbspQ:"+Q+
         "</p>";
+    }
+    
+    /*
+    * Creates error input element
+    */
+    function createErrElement(){
+        return "<p>Invalid input detected!</p><br/>";
     }
         
     /**
@@ -122,16 +136,14 @@ $(document).ready(()=>{
        
      /**
     * shifts each character to the right
-    * @param A {String} - 
-    * @param Q {String} - 
     * @param currA {String} - 
     * @param currQ {String} - 
     */
-    function shiftRight(A, Q, currA, currQ){
-        initCurrentVar(A, Q, currA, currQ);
-        shiftingAlgorithm(A, Q, currA, currQ);
-        A = currA.slice();
-        Q = currQ.slice();
+    function shiftRight(curra, currq){
+        initCurrentVar(A, Q, curra, currq);
+        shiftingAlgorithm(currA, currQ);
+        A = arrToString(currA);
+        Q = arrToString(currQ);
     }
     
     /**
@@ -142,14 +154,31 @@ $(document).ready(()=>{
     * tempA {charp[]} - a copy of currA
     * carry {char} - the carry bit or whatever you call it
     */
-    function shiftingAlgorithm(A, Q, currA, currQ){
-        // TODO: recreate
-        const tempQ = currQ.slice();
-        const tempA = currA.slice();
-        //incomplete loop
-//        for(var i = 0; i < tempA.length - 1; i++){
-//            currA[i + 1] = tempA[i];
-//        }
+    function shiftingAlgorithm(curra, currq){
+        // TODO: recreate to a more efficient code if possible
+        var lsb = getLSB(curra);
+        curra = shift(curra);
+        currq[0] = lsb;
+        C = getLSB(currq);
+        currq = shift(currq);
+        currA = curra;
+        currQ = currq;
+        A = arrToString(currA);
+        Q = arrToString(currQ);
+    }
+    
+    /*
+    * Part of the shiftingAlgorithm() function
+    * @param curr {String[]} - input to be shifted
+    * returns the shifted input
+    */
+    function shift(curr){
+        for(var i = 1; i < curr.length; i++){
+            curr[i] = curr[i-1];
+            console.log(curr);
+        }
+        console.log(curr);
+        return curr;
     }
     
     /**
